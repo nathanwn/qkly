394825556680d541fc5f39f189bbe4da14b940e2
nathan-wien <nhat.nguyen.cs17@gmail.com>
2022-06-14 13:09:27 +1000

    test: add a bunch of tests for judge id

--- internal/judges/judge_test.go
+++ internal/judges/judge_test.go
@@ -9,11 +9,39 @@ func TestJudgeId(t *testing.T) {
 	xJudgeNames[CodeForces{}] = "codeforces"
 	xJudgeNames[CSES{}] = "cses"
 	xJudgeNames[HackerRank{}] = "hackerrank"
+	xJudgeNames[SPOJ{}] = "spoj"
 
 	for judge, xId := range xJudgeNames {
 		t.Run(xId, func(t *testing.T) {
 			if judge.Id() != xId {
 				t.Fatalf("Wrong judge name: expected \"%s\", found %s\n", xId, judge.Id())
+			}
+		})
+	}
+}
+
+func TestGetJudge(t *testing.T) {
+	xJudges := make(map[string]string)
+
+	xJudges["https://atcoder.jp/contests/abc246/tasks/abc246_a"] = "atcoder"
+	xJudges["https://www.atcoder.jp/contests/abc246/tasks/abc246_a"] = "atcoder"
+	xJudges["https://codeforces.com/contest/547/problem/C"] = "codeforces"
+	xJudges["https://www.codeforces.com/contest/547/problem/C"] = "codeforces"
+	xJudges["https://codeforces.com/problemset/problem/1697/F"] = "codeforces"
+	xJudges["https://www.codeforces.com/problemset/problem/1697/F"] = "codeforces"
+	xJudges["https://cses.fi/problemset/task/1680/"] = "cses"
+	xJudges["https://www.cses.fi/problemset/task/1680/"] = "cses"
+	xJudges["https://spoj.com/problems/SCUBADIV/"] = "spoj"
+	xJudges["https://www.spoj.com/problems/SCUBADIV/"] = "spoj"
+
+	for url, xJudgeId := range xJudges {
+		t.Run(url, func(t *testing.T) {
+			judge, err := GetJudge(url)
+			if err != nil {
+				t.Fatalf("Unsupported judge for url\"%s\"\n", url)
+			}
+			if judge.Id() != xJudgeId {
+				t.Fatalf("Wrong judge name: expected \"%s\", found %s\n", xJudgeId, judge.Id())
 			}
 		})
 	}
